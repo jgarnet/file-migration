a471fbcc7dece32d2aff5d385c9aafaf0070e77a
Jalen Garnett <jalen.garnett@availity.com>
2025-08-11 12:53:33 -0400

    feat: allow global locking to run job on one container if needed

---
+++ src/main/java/org/example/GlobalScheduler.java
@@ -0,0 +1,130 @@
+package org.example;
+
+import org.example.configuration.ConfigurationProperties;
+import org.example.jobs.CleanupJob;
+import org.example.jobs.MigrationJob;
+import org.example.jobs.RetryJob;
+import org.example.jobs.SeedJob;
+import org.example.lock.Lock;
+import org.example.logger.Logger;
+import org.example.logger.SystemLogger;
+import org.example.mover.FileMover;
+import org.example.persistence.repository.FilesRepository;
+import org.example.persistence.repository.MigrationFilesRepository;
+import org.example.persistence.repository.MigrationRangesRepository;
+
+import java.util.UUID;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class GlobalScheduler implements Runnable {
+    private static final String GLOBAL_KEY = "MIGRATION_GLOBAL_LOCK";
+    private static final UUID GLOBAL_LOCK = UUID.randomUUID();
+    private final Logger log = new SystemLogger();
+    private final ConfigurationProperties config;
+    private final Lock lock;
+    private final FilesRepository filesRepository;
+    private final MigrationFilesRepository migrationFilesRepository;
+    private final MigrationRangesRepository rangesRepository;
+    private final FileMover fileMover;
+    private ScheduledExecutorService globalScheduler;
+    private ScheduledExecutorService jobScheduler;
+    private ScheduledExecutorService seedScheduler;
+    private ScheduledExecutorService retryScheduler;
+    private ScheduledExecutorService cleanupScheduler;
+    private boolean schedulersInitialized = false;
+
+    public GlobalScheduler(ConfigurationProperties config, Lock lock, FilesRepository filesRepository, MigrationFilesRepository migrationFilesRepository, MigrationRangesRepository rangesRepository, FileMover fileMover) {
+        this.config = config;
+        this.lock = lock;
+        this.filesRepository = filesRepository;
+        this.migrationFilesRepository = migrationFilesRepository;
+        this.rangesRepository = rangesRepository;
+        this.fileMover = fileMover;
+        this.globalScheduler = Executors.newSingleThreadScheduledExecutor();
+    }
+
+    @Override
+    public void run() {
+        if (this.config.getBoolean("ENABLE_GLOBAL_LOCKING", false)) {
+            try {
+                // only run job on one container during a given period to reduce load if needed
+                int globalJobTtl = this.config.getInteger("GLOBAL_LOCK_TTL", 7_200);
+                if (this.lock.acquireLock(GLOBAL_KEY, GLOBAL_LOCK, globalJobTtl)) {
+                    this.globalScheduler.submit(this::scheduleJobs);
+                } else {
+                    // job already locked by another container; wait to check again until lock period is over
+                    this.globalScheduler.schedule(this, globalJobTtl, TimeUnit.SECONDS);
+                    // in case this container had previous lock, shut down schedulers
+                    this.shutdownSchedulers();
+                }
+            } catch (Exception e) {
+                this.log.error(e);
+                this.globalScheduler.schedule(this, 300, TimeUnit.SECONDS);
+            }
+        } else {
+            this.globalScheduler.submit(this::scheduleJobs);
+        }
+    }
+
+    private void scheduleJobs() {
+        this.schedulersInitialized = true;
+
+        // Schedulers
+        int jobThreads = config.getInteger("JOB_THREADS", 20);
+        this.jobScheduler = Executors.newScheduledThreadPool(jobThreads);
+        int retryThreads = config.getInteger("RETRY_THREADS", 5);
+        this.retryScheduler = Executors.newScheduledThreadPool(retryThreads);
+        this.seedScheduler = Executors.newSingleThreadScheduledExecutor();
+        this.cleanupScheduler = Executors.newSingleThreadScheduledExecutor();
+
+        MigrationJob migrationJob = new MigrationJob(
+                this.config,
+                this.jobScheduler,
+                this.rangesRepository,
+                this.migrationFilesRepository,
+                this.filesRepository,
+                this.fileMover,
+                this.lock
+        );
+        SeedJob seedJob = new SeedJob(
+                this.config,
+                this.seedScheduler,
+                this.lock,
+                this.rangesRepository
+        );
+        RetryJob retryJob = new RetryJob(
+                this.config,
+                this.retryScheduler,
+                this.migrationFilesRepository,
+                this.fileMover
+        );
+        CleanupJob cleanupJob = new CleanupJob(
+                this.config,
+                this.cleanupScheduler,
+                this.lock,
+                this.rangesRepository
+        );
+
+        // Initialize
+        this.seedScheduler.submit(seedJob);
+        for (int i = 0; i < jobThreads; i++) {
+            this.jobScheduler.schedule(migrationJob, 30, TimeUnit.SECONDS);
+        }
+        for (int i = 0; i < retryThreads; i++) {
+            this.retryScheduler.schedule(retryJob, 30, TimeUnit.SECONDS);
+        }
+        this.cleanupScheduler.schedule(cleanupJob, 60, TimeUnit.SECONDS);
+    }
+
+    private void shutdownSchedulers() {
+        if (this.schedulersInitialized) {
+            this.jobScheduler.shutdown();
+            this.seedScheduler.shutdown();
+            this.retryScheduler.shutdown();
+            this.cleanupScheduler.shutdown();
+            this.schedulersInitialized = false;
+        }
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,10 +2,6 @@ package org.example;
 
 import org.example.configuration.ConfigurationProperties;
 import org.example.configuration.SystemConfigurationProperties;
-import org.example.jobs.CleanupJob;
-import org.example.jobs.MigrationJob;
-import org.example.jobs.RetryJob;
-import org.example.jobs.SeedJob;
 import org.example.lock.Lock;
 import org.example.mover.FileMover;
 import org.example.mover.StubFileMover;
@@ -14,10 +10,6 @@ import org.example.persistence.database.HikariDatabase;
 import org.example.persistence.repository.*;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
-
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) {
@@ -38,51 +30,15 @@ public class Main {
         MigrationRangesRepository rangesRepository = new PostgresMigrationRangesRepository(database, config);
         FileMover fileMover = new StubFileMover();
 
-        // Schedulers
-        int jobThreads = config.getInteger("JOB_THREADS", 20);
-        ScheduledExecutorService jobScheduler = Executors.newScheduledThreadPool(jobThreads);
-        int retryThreads = config.getInteger("RETRY_THREADS", 5);
-        ScheduledExecutorService retryScheduler = Executors.newScheduledThreadPool(retryThreads);
-        ScheduledExecutorService seedScheduler = Executors.newSingleThreadScheduledExecutor();
-        ScheduledExecutorService cleanupScheduler = Executors.newSingleThreadScheduledExecutor();
-
-        // jobs
-        MigrationJob migrationJob = new MigrationJob(
+        // Global Scheduler
+        GlobalScheduler globalScheduler = new GlobalScheduler(
                 config,
-                jobScheduler,
-                rangesRepository,
-                migrationFilesRepository,
-                filesRepository,
-                fileMover,
-                lock
-        );
-        SeedJob seedJob = new SeedJob(
-                config,
-                seedScheduler,
                 lock,
-                rangesRepository
-        );
-        RetryJob retryJob = new RetryJob(
-                config,
-                retryScheduler,
+                filesRepository,
                 migrationFilesRepository,
+                rangesRepository,
                 fileMover
         );
-        CleanupJob cleanupJob = new CleanupJob(
-                config,
-                cleanupScheduler,
-                lock,
-                rangesRepository
-        );
-
-        // Initialize
-        seedScheduler.submit(seedJob);
-        for (int i = 0; i < jobThreads; i++) {
-            jobScheduler.schedule(migrationJob, 30, TimeUnit.SECONDS);
-        }
-        for (int i = 0; i < retryThreads; i++) {
-            retryScheduler.schedule(retryJob, 30, TimeUnit.SECONDS);
-        }
-        cleanupScheduler.schedule(cleanupJob, 60, TimeUnit.SECONDS);
+        globalScheduler.run();
     }
 }
